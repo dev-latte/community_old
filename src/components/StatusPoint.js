cc75bbdf58ebf4bfa2a5dba3a7d2afa942ade600
dev-latte <dev.vlatte@gmail.com>
2021-09-29 11:08:18 +0900

    4.3 change status when user equip items

--- src/components/StatusPoint.js
+++ src/components/StatusPoint.js
@@ -17,8 +17,17 @@ const StatusPoint = ({ userObj }) => {
         point: 10,
         uid: userObj.uid
     };
+
     const [status, setStatus] = useState(initStatus);
-    const [equipment, setEquipment] = useState({});
+    const [equipment, setEquipment] = useState({
+        hp: 0,
+        def: 0,
+        atk: 0,
+        int: 0,
+        dex: 0,
+        agi: 0,
+        luk: 0,
+    });
 
     useEffect(() => {
         let isSubscribed = true;
@@ -27,6 +36,7 @@ const StatusPoint = ({ userObj }) => {
             firebaseAPI({ userObj }, process.env.REACT_APP_DB_STATUS, status);
             onRealtimeStatus();
             initEquipment();
+            onCurrentEquipment();
         }
         return () => isSubscribed = false;
     }, []);
@@ -54,6 +64,7 @@ const StatusPoint = ({ userObj }) => {
     }
 
     // init user eqiupment
+    // 유저 장착 정보가 없으면 초기화, 있으면 데이터값을 가져온다.
     const initEquipment = async () => {
         await firebaseAPI({ userObj }, process.env.REACT_APP_DB_USER_EQUIPMENT, {
             uid: userObj.uid,
@@ -61,14 +72,44 @@ const StatusPoint = ({ userObj }) => {
             armor: {},
             jewelry: {}
             }).then(data => {
-                setEquipment(data);
-                // 아이템들의 능력치를 합해서 하나로 만들어두기
-                // 장착정보 스냅샷을 여기에 둬야하는건 아닌지?
-                console.log(data);
+                const equipmentStatus = {};
+                for(let key in data) {
+                    if(key !== "uid"){
+                        const dataObj = data[key];
+                        if(Object.keys(dataObj).length !== 0) {
+                            for(let key in equipment) {
+                                equipmentStatus[key] = (equipmentStatus[key] === undefined ? 0 : equipmentStatus[key]) + dataObj[key];
+                            }
+                        }
+                    }
+                }
+                setEquipment(equipmentStatus);
             }).catch(error => {
                 console.log(`init Equipment Error ${error}`);
             });
     }
+
+    // 착용정보가 변하면 적용되는 데이터도 변한다.
+    const onCurrentEquipment = async () => {
+        await dbService.collection(process.env.REACT_APP_DB_USER_EQUIPMENT).doc(userObj.uid).onSnapshot((snap) => {
+            if(snap.exists){
+                const data = snap.data();
+                const equipmentStatus = {};
+                for(let key in data) {
+                    if(key !== "uid"){
+                        const dataObj = data[key];
+                        if(Object.keys(dataObj).length !== 0) {
+                            for(let key in equipment) {
+                                equipmentStatus[key] = (equipmentStatus[key] === undefined ? 0 : equipmentStatus[key]) + dataObj[key];
+                            }
+                        }
+                    }
+                }
+                setEquipment(equipmentStatus);
+            }
+        });
+    }
+
 
     const onClickPlus = async (e) => {
         if(status.point < 1) { return; }
@@ -122,22 +163,22 @@ const StatusPoint = ({ userObj }) => {
             <div>공격력: {status.atk * 10}</div>
             <div>방어력: {status.def * 5}</div>
             <ul>
-                <li>ATK: { status.atk }
+                <li>ATK: { status.atk } {equipment.atk !== 0 && <span>( +{ equipment.atk })</span> }
                     {status.point !== 0 && <button onClick={onClickPlus} value="atk">+</button>}
                 </li>
-                <li>INT: {status.int}
+                <li>INT: {status.int} {equipment.int !== 0 && <span>( +{ equipment.int })</span> }
                     {status.point !== 0 && <button onClick={onClickPlus} value="int">+</button>}
                 </li>
-                <li>DEF: {status.def}
+                <li>DEF: {status.def} {equipment.def !== 0 && <span>( +{ equipment.def })</span> }
                     {status.point !== 0 && <button onClick={onClickPlus} value="def">+</button>}
                 </li>
-                <li>DEX: {status.dex}
+                <li>DEX: {status.dex} {equipment.dex !== 0 && <span>( +{ equipment.dex })</span> }
                     {status.point !== 0 && <button onClick={onClickPlus} value="dex">+</button>}
                 </li>
-                <li>AGI: {status.agi}
+                <li>AGI: {status.agi} {equipment.agi !== 0 && <span>( +{ equipment.agi })</span> }
                     {status.point !== 0 &&<button onClick={onClickPlus} value="agi">+</button>}
                 </li>
-                <li>LUK: {status && status.luk}
+                <li>LUK: {status.luk} {equipment.luk !== 0 && <span>( +{ equipment.luk })</span> }
                     {status.point !== 0 && <button onClick={onClickPlus} value="luk">+</button>}
                 </li>
                 <li>잔여 포인트: {status.point}</li>
